bec254342a5e375c9e544135de053527044e335f
Satyam yadav <sy312607@gmail.com>
2026-02-25 00:17:55 +0530

    Remove AgoraConfig from tracking, add to gitignore

--- app/src/main/java/com/example/studysync/AgoraConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.studysync;
-
-public class AgoraConfig {
-    // REPLACE THIS WITH YOUR ACTUAL APP ID FROM AGORA CONSOLE
-    public static final String APP_ID ="81ec342bbf0c46b29c35f2e85e547799";
-
-    // Keep this as null for testing
-    public static final String TOKEN = null;
-}
